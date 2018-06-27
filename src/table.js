a5b841d6b67f3a4448ad8c79359c30d874f1885f
Unknown <marcus.jones@riventechnologies.com>
2018-06-27 11:58:27 -0700

    Reordered columns and added autofit function

--- src/table.js
+++ src/table.js
@@ -11,82 +11,39 @@ class GridExample extends Component {
             //todo add column defs api call
             columnDefs: [
                 {
-                    headerName: "symbol",
+                    headerName: "Pair",
                     field: "symbol"
                 },
                 {
-                    headerName: "price",
+                    headerName: "Current Price",
                     field: "price"
                 },
                 {
-                    headerName: "bought_price",
+                    headerName: "Bought Price",
                     field: "bought_price"
                 },
                 {
-                    headerName: "percent_change",
+                    headerName: "Percent Change",
                     field: "percent_change"
                 },
                 {
-                    headerName: "volume",
-                    field: "volume"
-                },
-                {
-                    headerName: "change24",
+                    headerName: "24H Change",
                     field: "change24"
                 },
                 {
-                    headerName: "DCA_level",
+                    headerName: "DCA Level",
                     field: "DCA_level"
                 },
                 {
-                    headerName: "MFI_24",
+                    headerName: "Volume",
+                    field: "volume"
+                },
+                {
+                    headerName: "MFI 24H",
                     field: "MFI_24"
-                }
+                },
             ],
-            rowData: [
-                {
-                    id: "aa",
-                    make: "Toyota",
-                    model: "Celica",
-                    price: 35000
-                },
-                {
-                    id: "bb",
-                    make: "Ford",
-                    model: "Mondeo",
-                    price: 32000
-                },
-                {
-                    id: "cc",
-                    make: "Porsche",
-                    model: "Boxter",
-                    price: 72000
-                },
-                {
-                    id: "dd",
-                    make: "BMW",
-                    model: "5 Series",
-                    price: 59000
-                },
-                {
-                    id: "ee",
-                    make: "Dodge",
-                    model: "Challanger",
-                    price: 35000
-                },
-                {
-                    id: "ff",
-                    make: "Mazda",
-                    model: "MX5",
-                    price: 28000
-                },
-                {
-                    id: "gg",
-                    make: "Horse",
-                    model: "Outside",
-                    price: 99000
-                }
-            ],
+            rowData: [],
             defaultColDef: { editable: false },
             getRowNodeId: function(data) {
                 return data.id;
@@ -96,7 +53,7 @@ class GridExample extends Component {
 
     onGridReady(params) {
         this.gridApi = params.api;
-        this.gridColumnApi = params.columnApi;
+        this.gridColumnApi = params.api.sizeColumnsToFit();
     }
 
     updateSort() {
@@ -105,23 +62,7 @@ class GridExample extends Component {
     updateFilter() {
         this.gridApi.refreshClientSideRowModel("filter");
     }
-    setPriceOnToyota() {
-        var rowNode = this.gridApi.getRowNode("aa");
-        var newPrice = Math.floor(Math.random() * 100000);
-        rowNode.setDataValue("price", newPrice);
-    }
-    setDataOnFord() {
-        var rowNode = this.gridApi.getRowNode("bb");
-        var newPrice = Math.floor(Math.random() * 100000);
-        var newModel = "T-" + Math.floor(Math.random() * 1000);
-        var newData = {
-            id: "bb",
-            make: "Ford",
-            model: newModel,
-            price: newPrice
-        };
-        rowNode.setData(newData);
-    }
+
     componentDidMount() {
               fetch('https://gist.githubusercontent.com/K-Kit/d3fe07c5430c76b92a8585fc11baf49f/raw/3af793758a3ed6f6260e0e0017c74b6a0078499f/apx.json')
                   .then(result => result.json())
@@ -129,14 +70,14 @@ class GridExample extends Component {
             }
     render() {
         return (
-            <div style={{ width: "auto", height: "900px" }}>
+            <div style={{width: "auto", height: "900px" }}>
                 <div style={{ height: "100%", boxSizing: "border-box" }}>
                     <div
                         id="myGrid"
                         style={{
                             boxSizing: "border-box",
                             height: "100%",
-                            width: "100%"
+                            width: "100%",
                         }}
                         className="ag-theme-balham-dark"
                     >
